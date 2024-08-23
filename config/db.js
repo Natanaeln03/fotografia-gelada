f09ae35244d89516b2a9dbd6c9648ddc2cccfa80
Natanael <natancnascimento88@gmail.com>
2024-08-23 10:17:29 -0300

    modificado

--- config/db.js
+++ config/db.js
@@ -1,5 +1,18 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    
+    host:process.env.DB_HOST, 
+    user:process.env.DB_USER, 
+    password:process.env.DB_PASS, 
+    database:process.env.DB_NAME
 });
+
+db.connect((err) => {
+    if(err){
+        console.error('erro ao conectar ao Banco de Dados.', err); 
+    return;
+}
+    console.log(`conectado ao Banco de Dados Mysql, ${process.env.DB_NAME}`); 
+});
+
+module.exports=db;
